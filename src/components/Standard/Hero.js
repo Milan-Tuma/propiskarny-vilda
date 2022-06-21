52d9faf397638f49569b6e55d01667b233c0ca13
Milan Tůma <milant6@seznam.cz>
2022-06-21 08:17:55 +0200

    Update for hero component

--- src/components/Standard/Hero.js
+++ src/components/Standard/Hero.js
@@ -1,29 +1,46 @@
+import { StdButton } from '../UI/Buttons';
+
 import classes from './Hero.module.css';
 
 const heroData = {
 	en: {
 		imageAlt: 'Hero image',
-		header: 'Writing history since 2010',
-		subheader:
+		header: 'Individualize pens according to your needs',
+		subheader: 'All products can be further customized.',
+		btn: 'Visit our e-shop',
+		aboutUs:
 			'Our journey started with a high school project for two students, who turned out to be co-founders of pen manufacturing company Vilda Ltd. Since then the company faced many challenges and opportunities until we became a stable manufacturer in the market with the highest competitiveness.',
 	},
 	cs: {
 		imageAlt: 'Úvodní obrázek',
-		header: 'Píšeme historii od 2010',
-		subheader:
+		header: 'Unikátní propisky dle vašich představ',
+		subheader: 'Všechny produkty je možné dále upravit.',
+		btn: 'Navštivte náš e-shop',
+		aboutUs:
 			'Naše cesta započala středoškolním projektem pro dva studenty, kteří se stali spoluzakladateli Propiskáren Vilda s.r.o. Od této chvíle čelila naše společnost mnoha výzvám a příležitostem, až se nakonec stala stabilním výrobcem na trhu s nejvyšší konkurenceschopností.',
 	},
 };
 
 const Hero = ({ lang }) => {
+	const heroHeader = heroData[lang].header.split(' ');
+	const partOne = heroHeader.slice(0, 2).join(' ');
+	const partTwo = heroHeader.slice(2).join(' ');
+
+	const btnClickScroll = () => {
+		const height = window.innerHeight;
+		window.scrollBy(0, height);
+	};
+
 	return (
-		<div className={classes.wrapper}>
-			<div className={classes.image}>
-				<img src="/layout/hero-image.jpg" alt={heroData[lang].imageAlt} />
-			</div>
-			<div className={classes.text}>
-				<h1>{heroData[lang].header}</h1>
-				<p>{heroData[lang].subheader}</p>
+		<div className={classes.background}>
+			<div className={classes.content}>
+				<h1 className={classes.header}>
+					{partOne} <span>{partTwo}</span>
+				</h1>
+				<div className={classes.btn} onClick={btnClickScroll}>
+					<StdButton>{heroData[lang].btn}</StdButton>
+				</div>
+				<p className={classes.subheader}>{heroData[lang].subheader}</p>
 			</div>
 		</div>
 	);
